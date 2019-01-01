9b0d11a20a00f0498cf749db93203964f9d921cb
liuyuchun <2786161648@qq.com>
2019-01-01 12:06:27 +0800

    keep-alive&activated

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="home">
-      <home-header :city="city"></home-header>
-      <home-swiper :swiperList="swiperList"></home-swiper>
-      <icon :iconList="iconList"></icon>
-      <recommend :recommendList="recommendList"></recommend>
-      <weekend :weekendList="weekendList"></weekend>
-    </div>
+  <div class="home">
+    <home-header></home-header>
+    <home-swiper :swiperList="swiperList"></home-swiper>
+    <icon :iconList="iconList"></icon>
+    <recommend :recommendList="recommendList"></recommend>
+    <weekend :weekendList="weekendList"></weekend>
+  </div>
 </template>
 
 <script>
@@ -14,49 +14,63 @@
   import icon from "./components/icon"
   import recommend from "./components/recommend"
   import weekend from "./components/weekend"
-    export default {
-        name: "home",
-        data () {
-          return {
-            city:'',
-            swiperList:[],
-            iconList:[],
-            recommendList:[],
-            weekendList:[]
-          }
-        },
-        components:{
-          homeHeader,
-          homeSwiper,
-          icon,
-          recommend,
-          weekend
+  import { mapState } from 'vuex'
+  export default {
+    name: "home",
+    data() {
+      return {
+        swiperList: [],
+        iconList: [],
+        recommendList: [],
+        weekendList: [],
+        lastCity:''
+      }
+    },
+    computed: {
+      ...mapState(['city']),
+    },
+    components: {
+      homeHeader,
+      homeSwiper,
+      icon,
+      recommend,
+      weekend
 
-        },
-        mounted () {
-            this.axios.get("http://localhost:8080/api/index.json")
-              .then(res => {
-                console.log(res);
-                const result=res.data;
-                if (result.ret&&result.data) {
-                  console.log(result);
-                  this.city=result.data.city;
-                  this.swiperList=result.data.swiperList;
-                  this.iconList=result.data.iconList;
-                  this.recommendList=result.data.recommendList;
-                  this.weekendList=result.data.weekendList;
+    },
+    methods: {
+      getHomeinfo() {
+        this.axios.get("/api/index.json?city="+this.city).then(this.getHomeinfoSucc)
 
-                }
-              })
+      },
+      getHomeinfoSucc(res) {
+        console.log(res);
+        const result = res.data;
+        if (result.ret && result.data) {
+          console.log(result);
+          this.swiperList = result.data.swiperList;
+          this.iconList = result.data.iconList;
+          this.recommendList = result.data.recommendList;
+          this.weekendList = result.data.weekendList;
+
         }
+      }
+    },
+    mounted() {
+      this.getHomeinfo()
+      this.lastCity=this.city
+    },
+    activated() {
+      if( this.lastCity !== this.city ) {
+        this.getHomeinfo()
+        this.lastCity=this.city;
+      }
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
   .home
     background-color #eee
-
-
 
 
 </style>
