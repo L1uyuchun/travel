26c3679da0ffc59d55b1fe833532807da87f7175
liuyuchun <2786161648@qq.com>
2019-01-01 18:26:09 +0800

    banner finish

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="detail">
+    <div class="banner">
+      <img class="banner-img" src="@/assets/img/detail/gugong.jpg" alt="">
+      <i class="iconfont goback">&#xe604;</i>
+      <div class="infor">
+        <p class="title">故宫（AAAAA级景区）</p>
+        <p class="imgNum"><i class="iconfont iconSize">&#xe67e;</i>8</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Detail"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    position relative
+
+    .banner-img
+      display block
+      width 100%
+
+    .goback
+      position absolute
+      top: 0.1rem
+      left 0.1rem
+      color #ffffff
+      background-color rgba(0, 0, 0, 0.4)
+      font-size 0.4rem
+      width 0.5rem
+      height 0.5rem
+      line-height 0.5rem
+      border-radius 50%
+      text-align center
+    .infor
+      position absolute
+      width 100%
+      left: 0
+      bottom 0rem
+      color #ffffff
+      line-height: .7rem
+      font-size 0.4rem
+      background-image linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, .4))
+
+    .title
+      padding 0rem 1.3rem 0rem 0.3rem
+
+    .imgNum
+      width 0.6rem
+      padding 0rem 0.25rem
+      height 0.4rem
+      line-height 0.4rem
+      font-size: 0.25rem;
+      border-radius 0.2rem
+      background-color rgba(0, 0, 0, .4)
+      position absolute
+      right 0.2rem
+      bottom 0.05rem
+
+      .iconSize
+        font-size 0.2rem
+        margin-right 0.1rem
+
+
+</style>
--- src/pages/home/components/recommend.vue
+++ src/pages/home/components/recommend.vue
@@ -1,77 +1,99 @@
 <template>
-    <div class="recommend">
-     <p class="love">猜你喜欢</p>
-      <div class="point border-bottom" v-for="item in recommendList" :key="item.id">
-        <img :src="item.imgUrl" alt="" class="pointImg">
-        <div class="rightText">
-          <p class="title">{{item.title}}</p>
-          <p class="comment"><span class="star"><img src="~@/assets/img/homeImg/star.png" alt="" v-for="item in 5"></span>{{item.commentMount}}条评论</p>
-          <p class="price">
-            <span class="symbol">&yen;</span>
-            <span class="mount">{{item.mount}}</span>
-            <span class="text">起</span>
-            <span class="addr">金牛区</span>
-          </p>
-          <p class="intro">{{item.desc}}</p>
-        </div>
+  <div class="recommend">
+    <p class="love">猜你喜欢</p>
+    <div class="point border-bottom"
+         v-for="item in recommendList"
+         :key="item.id"
+         @click="goDetail(item.id)"
+    >
+      <img :src="item.imgUrl" alt="" class="pointImg">
+      <div class="rightText">
+        <p class="title">{{item.title}}</p>
+        <p class="comment"><span class="star"><img src="~@/assets/img/homeImg/star.png" alt="" v-for="item in 5"></span>{{item.commentMount}}条评论
+        </p>
+        <p class="price">
+          <span class="symbol">&yen;</span>
+          <span class="mount">{{item.mount}}</span>
+          <span class="text">起</span>
+          <span class="addr">金牛区</span>
+        </p>
+        <p class="intro">{{item.desc}}</p>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "recommend",
-        props:['recommendList']
+  export default {
+    name: "recommend",
+    props: ['recommendList'],
+    methods: {
+      goDetail(id) {
+        this.$router.push({ path:'/detail' });
+      }
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
   @import "~styles/sllipse.styl"
   .recommend
     background-color #ffffff;
-    padding-left:0.2rem
+    padding-left: 0.2rem
+
     .love
       height 1rem
       line-height 1rem
       font-size 0.35rem
+
     .point
-      padding-top:0.2rem
+      padding-top: 0.2rem
       position relative
+
       .pointImg
         display block
         width 2rem
         height 2rem
         position absolute
+
       .rightText
-         padding-left 2.5rem
-         .title
-           ellipse()
-           font-size 0.3rem
-           padding-top: 0.2rem;
-         .comment
-           line-height 0.7rem
-           font-size 0.2rem
-           color #666
-           .star
-             display inline
-             vertical-align top
-             margin-right:0.2rem
-         .price
-            height 0.4rem
-            padding-top 0.4rem
-            .symbol
-               color #ff9b1b
-            .mount
-              font-size 0.4rem
-              color #ff9b1b
-            .addr
-               float right
-               padding-right:0.2rem
-         .intro
-            line-height 1rem
-            color red
-            ellipse()
-            min-width 0rem
+        padding-left 2.5rem
+
+        .title
+          ellipse()
+          font-size 0.3rem
+          padding-top: 0.2rem;
+
+        .comment
+          line-height 0.7rem
+          font-size 0.2rem
+          color #666
+
+          .star
+            display inline
+            vertical-align top
+            margin-right: 0.2rem
+
+        .price
+          height 0.4rem
+          padding-top 0.4rem
+
+          .symbol
+            color #ff9b1b
+
+          .mount
+            font-size 0.4rem
+            color #ff9b1b
+
+          .addr
+            float right
+            padding-right: 0.2rem
+
+        .intro
+          line-height 1rem
+          color red
+          ellipse()
+          min-width 0rem
 
 
 </style>
