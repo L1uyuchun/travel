58d9132b4854c7ec514e25d0d00bfcfe1516c9ac
liuyuchun <2786161648@qq.com>
2019-01-01 22:38:26 +0800

    gallery finish

---
+++ src/common/gallery/gallery.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="gallery" @click="handleGalleryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="(item,index) in galleryImg" :key="index">
+        <img class="swiper-img" :src="item" alt="">
+      </swiper-slide>
+
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "gallery",
+    props:{
+      'galleryImg':Array
+    },
+    data() {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          paginationType: "fraction",
+          loop:true,
+          observer:true,
+          observeParents:true,
+        }
+      }
+    },
+    methods: {
+      handleGalleryClick() {
+        this.$emit('galleryHidden',false);
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-container
+     overflow inherit
+  .gallery
+    z-index 99
+    position fixed
+    top:0
+    bottom 0
+    left:0
+    right: 0
+    background-color #000
+    display flex
+    flex-direction column
+    justify-content center
+    .wrapper
+      height 0
+      padding-bottom 66.5%
+      background-color #ffffff
+      .swiper-img
+         width 100%
+      .swiper-pagination
+         bottom -1rem
+         color:#ffffff
+
+
+
+</style>
